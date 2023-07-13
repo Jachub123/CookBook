2da7c472050baa8f0262d2e7a7695959faf9c8c3
Jakob Nonnenmacher <jnonnenmacher@datec24.ag>
2023-07-13 17:07:53 +0200

    added Files

---
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.scss'],
+})
+export class RecipeEditComponent {
+  constructor(private route: ActivatedRoute) {}
+  id: number;
+  editMode: boolean = false;
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
+  }
+}
